a570ab7512588d3672cd341c26cfb486e889951c
Unknown <ville.sutinen@metropolia.fi>
2019-03-31 00:25:44 +0200

    Things and Stuff
    animaatioit, kamera korjauksia, itse kenttää rakenneltu, ties mitä

---
+++ Basic_Things2 – kopio/Assets/Scripts/PickUp.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, 50) * Time.deltaTime);
+    }
+}
